356709d10c3eb5fe2e14cffaeb7cb16009493fda
Christopher Bailey <cbailey@mort.is>
2019-05-20 07:50:09 -0400

    Updates clasifier

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     author="Christopher Bailey",
     author_email="cbailey@mort.is",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
